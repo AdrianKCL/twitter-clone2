03e1b10ad04ff237789a2033b20c0085815f903a
AdrianKCL <92375484+AdrianKCL@users.noreply.github.com>
2022-06-16 18:41:47 +0200

    Some changes

--- components/Input.tsx
+++ components/Input.tsx
@@ -6,16 +6,32 @@ import {
   XIcon,
 } from "@heroicons/react/outline";
 import React, { useRef, useState } from "react";
+import { db, storage } from "../firebase";
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  updateDoc,
+} from "@firebase/firestore";
+import { getDownloadURL, ref, uploadString } from "@firebase/storage";
 export default function Input() {
   const [input, setInput] = useState("");
   const [selectedFile, setSelectedFile] = useState(null);
   const filePickerRef = useRef(null);
   const [loading, setLoading] = useState(false);
   const [showEmojis, setShowEmojis] = useState(false);
-  const sendPost = () => {
+  const sendPost = async () => {
     if (loading) return;
     setLoading(true);
-    const docRef = 
+    const docRef = await addDoc(collection(db, "posts"), {
+      // id: session.user.uid,
+      // username: session.user.name,
+      // userImg: session.user.image,
+      // tag: session.user.tag,
+      text: input,
+      timestamp: serverTimestamp(),
+    });
   };
   const addImageToPost = () => {};
 
--- firebase.js
+++ firebase.js
@@ -1,3 +1,12 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCWF1jzMFjOAy3VbVLaDaIJVBeSsef0EQQ",
   authDomain: "twitter-clone-c679e.firebaseapp.com",
@@ -6,3 +15,11 @@ const firebaseConfig = {
   messagingSenderId: "1032880403701",
   appId: "1:1032880403701:web:fc2edc0e17b8e9dbe8c1bc",
 };
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const storage = getStorage();
+
+export default app;
+export { db, storage };
