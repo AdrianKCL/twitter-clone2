ce1d961b3971c5d4c94ecb97185779ed357de33d
AdrianKCL <92375484+AdrianKCL@users.noreply.github.com>
2022-06-20 08:24:36 +0200

    Some changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
-
-export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,12 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
+import { getProviders, getSession, useSession } from "next-auth/react";
+import Login from "../components/Login";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ trendingResults, followResults, providers }: any) => {
+  const { data: session } = useSession();
+  if (!session) return <Login providers={providers} />;
   return (
     <div>
       <Head>
@@ -19,3 +23,22 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+export async function getServerSideProps(context: any) {
+  const trendingResults = await fetch("https://jsonkeeper.com/b/NKEV").then(
+    (res) => res.json()
+  );
+  const followResults = await fetch("https://jsonkeeper.com/b/SK3E").then(
+    (res) => res.json()
+  );
+  const providers = await getProviders();
+  const session = await getSession(context);
+
+  return {
+    props: {
+      trendingResults,
+      followResults,
+      providers,
+      session,
+    },
+  };
+}
